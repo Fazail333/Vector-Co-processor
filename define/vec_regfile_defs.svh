228f36b347d88397375520e087d91aec06774fa4
Zawaher-Bin-Asim <zawaherbinasim.333@gmail.com>
2024-09-16 01:56:48 +0500

    Updated the regfile_defs

--- define/vec_regfile_defs.svh
+++ define/vec_regfile_defs.svh
@@ -1,29 +1,26 @@
-`include "vector_processor_defs.svh"
+//Author        : Zawaher Bin Asim , UET Lahore
+//Description   : This the header file  for  register file of the vector processor
+// Date         : 13 Sep, 2024.
+
 
 `ifndef vec_regfile_defs
 `define vec_regfile_defs
 
-// LMUL to increase the venctor register length so that more vectors cna be stored  set it to 2 , 4   
-`define LMUL 1
+`include "vector_processor_defs.svh"
 
-// The number of vectors that one vector register should store
-`define LENGTH_OF_VECREG  256
+// The maximum length of the vector register
+`define   MAX_VLEN  4096  // Example maximum VLEN (adjust as needed)
 
-// The number of vector registers the register file will have
-parameter VECTOR_REGISTERS = (1/LMUL) * 32 ;
-
-// This is the width of the vector register
-parameter VLEN = `LMUL * LENGTH_OF_VECREG ;
-
-// No of vector registers
-// If LMUL is 2 the size of the vector register length increases but the number of the vector registers decreases by that factor
-parameter NO_OF_VEC_REGISTERS = (1/`LMUL) * 32 ;
-
+// Maximum number of vector registers (32 in total)
+`define   MAX_VEC_REGISTERS  32
 
 // The width of the address based on the architecture of the instruction
-parameter   ADDR_WIDTH = `XLEN ;
+parameter   ADDR_WIDTH = `XLEN;
 
-parameter   DATA_WIDTH = VLEN ;
+// The data width is based on the maximum VLEN, adjusted dynamically in the module
+parameter   DATA_WIDTH = `MAX_VLEN;
 
+// The width of the variable that tells the width of the vector register that is useful
+parameter   VECTOR_LENGTH = $clog2(`MAX_VLEN);
 
 `endif
