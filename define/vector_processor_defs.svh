dadd30e4aa314972301daf136eb2920e0d5ef04e
Zawaher-Bin-Asim <143878349+Zawaher-Bin-Asim@users.noreply.github.com>
2024-09-23 23:37:58 +0500

    added the data with parameter

--- define/vector_processor_defs.svh
+++ define/vector_processor_defs.svh
@@ -3,7 +3,10 @@
 
 // The architecture of the processor 32 bit or 64 bit 
 `define XLEN 32
-
+ 
+// The width of the vector register in the register file inspite of the lmul  
 `define VLEN 512
+// The width of the data signals . it depends  upon the "VLEN * max(lmul)" here the max of lmul is 8 
+`define MAX_VLEN 4096
 
 `endif
