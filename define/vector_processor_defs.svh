ac41eaa5f6811670ab677d853dd20e98475fcc64
Zawaher-Bin-Asim <zawaherbinasim.333@gmail.com>
2025-05-24 02:58:52 +0500

    Updated defs for the AXI 4 addition

--- define/vector_processor_defs.svh
+++ define/vector_processor_defs.svh
@@ -5,6 +5,8 @@
 `ifndef vector_processor_defs
 `define vector_processor_defs
 
+`include "../AXI-4/define/axi_4_defs.svh"
+
 // The architecture of the processor 32 bit or 64 bit 
 `define XLEN 32
  
@@ -13,9 +15,9 @@
 // The width of the data signals . it depends  upon the "VLEN * max(lmul)" here the max of lmul is 8 
 `define MAX_VLEN 4096
 // The width of the memory data bus
-`define DATA_BUS 128
+`define DATA_BUS    `DATA_BUS_WIDTH
 // Write stobe for memory
- parameter WR_STROB = $clog2(`DATA_BUS/8) ;
+ parameter WR_STROB = STROBE_WIDTH ;
 
 typedef enum logic [1:0]{  
     IDLE,
